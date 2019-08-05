41a56bd529176a539ce2060bbb49ceb9c4daad14
minicatsCB <minicatsCB@noreply.github.com>
2019-08-05 23:21:22 +0200

    Add GET endpoint to get the data saved until now

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({
-    extended: true
-}));
+const controller = require("./lib/controller");
+
+app.use(express.json());
+
+app.get("/data", (req, res) => {
+    let data = controller.getData();
+    console.log("Saved data until now:", data);
+    res.send(data);
+});
 
 app.post("/", (req, res) => {
-    console.log(req.body);
+    console.log("Received data:", JSON.stringify(req.body));
+    controller.saveData(JSON.stringify(req.body));
     res.send("Hello World!");
 });
 
