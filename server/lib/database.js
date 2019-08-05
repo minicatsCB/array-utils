7b72d91a6951cbd379f009aa0a1b9c1b185aa3c1
minicatsCB <minicatsCB@noreply.github.com>
2019-08-05 23:20:26 +0200

    Implement database function to get and save data from and in it

---
+++ server/lib/database.js
@@ -0,0 +1,23 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('database/db.json');
+const db = low(adapter);
+
+// Set some defaults (required if the JSON file is empty)
+db.defaults({ data: [] }).write();
+
+let database = {
+    getData: function() {
+        console.log("Getting data from database...");
+        return db.get("data").value();
+    },
+    saveData: function(data) {
+        console.log("Writing data to database:", data);
+         db.get("data")
+           .push(data)
+           .write();
+    }
+}
+
+module.exports = database;
