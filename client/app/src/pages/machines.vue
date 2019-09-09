579163993918200c3103e2b7f010abe7c1589f60
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-09-10 00:29:47 +0200

    Implement instructions section in master view

--- client/app/src/pages/machines.vue
+++ client/app/src/pages/machines.vue
@@ -1,9 +1,15 @@
 <template>
   <q-page>
-    <div class="q-pa-md row items-start q-gutter-md">
-        <machine-card></machine-card>
-        <machine-card></machine-card>
-        <machine-card></machine-card>
+    <div class="row q-pa-md">
+        <div class="col-6">
+            <machine-card></machine-card>
+            <machine-card></machine-card>
+            <machine-card></machine-card>
+        </div>
+        <div class="col-6">
+            <p>Select a machine from the list to see its details</p>
+            <img class="instructions-img" src="https://cdn.quasar.dev/img/mountains.jpg">
+        </div>
     </div>
   </q-page>
 </template>
@@ -23,5 +29,7 @@ export default {
 </script>
 
 <style>
-
+.instructions-img {
+    width: 200px;
+}
 </style>
