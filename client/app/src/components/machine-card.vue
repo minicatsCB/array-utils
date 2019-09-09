8da8d8bfd76511f7037339fecc3810d1b5ac454d
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-09-10 00:25:04 +0200

    Move machine card to its own component

---
+++ client/app/src/components/machine-card.vue
@@ -0,0 +1,32 @@
+<template>
+    <q-card class="my-card">
+      <q-card-section>
+        <q-icon name="warning" size="md" />
+      </q-card-section>
+      <q-card-section class="summary absolute-bottom text-right text-body2">
+        <div>ubuntu1</div>
+        <div>10.5.3.1</div>
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+export default {
+  name: 'machine-card'
+}
+</script>
+
+<style>
+.my-card {
+  width: 100%;
+  max-width: 150px;
+  height: 150px;
+  max-height: 150px;
+  display: inline-block;
+}
+
+.q-card .summary {
+  padding-top: 16px;
+  background-color: yellow;
+}
+</style>
--- client/app/src/pages/machines.vue
+++ client/app/src/pages/machines.vue
@@ -1,58 +1,27 @@
 <template>
   <q-page>
     <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card">
-        <q-card-section>
-          <q-icon name="warning" size="md" />
-        </q-card-section>
-        <q-card-section class="summary absolute-bottom text-right text-body2">
-          <div>ubuntu1</div>
-          <div>10.5.3.1</div>
-        </q-card-section>
-      </q-card>
-
-      <q-card class="my-card">
-        <q-card-section>
-          <q-icon name="warning" size="md" />
-        </q-card-section>
-        <q-card-section class="summary absolute-bottom text-right text-body2">
-          <div>ubuntu1</div>
-          <div>10.5.3.1</div>
-        </q-card-section>
-      </q-card>
-
-      <q-card class="my-card">
-        <q-card-section>
-          <q-icon name="warning" size="md" />
-        </q-card-section>
-        <q-card-section class="summary absolute-bottom text-right text-body2">
-          <div>ubuntu1</div>
-          <div>10.5.3.1</div>
-        </q-card-section>
-      </q-card>
+        <machine-card></machine-card>
+        <machine-card></machine-card>
+        <machine-card></machine-card>
     </div>
   </q-page>
 </template>
 
 <script>
+import MachineCard from '../components/machine-card'
+
 export default {
   name: 'machines',
   data () {
     return {}
+  },
+  components: {
+    MachineCard
   }
 }
 </script>
 
 <style>
-.my-card {
-  width: 100%;
-  max-width: 150px;
-  height: 150px;
-  max-height: 150px;
-}
 
-.q-card .summary {
-  padding-top: 16px;
-  background-color: yellow;
-}
 </style>
