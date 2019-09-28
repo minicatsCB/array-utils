73ee87dd966ebc0d58b5d34136f41931e2d3fc2b
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-09-29 01:28:29 +0200

    Modify main header title

--- client/app/src/layouts/Index.vue
+++ client/app/src/layouts/Index.vue
@@ -2,11 +2,8 @@
 <q-layout view="hHh lpr fff">
   <q-header elevated class="bg-purple text-white" height-hint="98">
     <q-toolbar>
-        <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-        </q-avatar>
-      <q-toolbar-title>
-        Title
+      <q-toolbar-title class="text-center text-uppercase text-h3 q-pa-xs">
+        Hacker's dashboard
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
