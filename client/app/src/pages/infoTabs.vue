11827798548f35f036e9abc29faf6e5d8480bc73
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-10-02 00:31:27 +0200

    Refactor info-tabs page

--- client/app/src/pages/infoTabs.vue
+++ client/app/src/pages/infoTabs.vue
@@ -1,66 +1,83 @@
 <template>
 <div class="q-pa-md">
   <div class="q-gutter-y-md">
-    <q-tabs v-model="tab" dense class="text-lime" active-color="lime" indicator-color="lime" align="justify" narrow-indicator>
+    <q-tabs v-model="tab" dense class="text-lime" active-color="lime" indicator-color="lime" align="center" narrow-indicator>
       <q-tab name="os" label="OS"></q-tab>
       <q-tab name="env" label="ENV"></q-tab>
     </q-tabs>
 
-    <q-separator class="bg-red"></q-separator>
+    <q-separator class="bg-lime"></q-separator>
 
     <q-tab-panels v-model="tab" animated>
       <q-tab-panel name="os">
-        <q-list v-if="machineData" bordered class="rounded-borders">
-          <q-expansion-item expand-separator icon="settings_system_daydream" label="Operative system" default-opened>
-            <q-item><span>Hostname: </span> {{machineData.data.os.hostname}}</q-item>
-            <q-item><span>Platform: </span> {{machineData.data.os.platform}}</q-item>
-            <q-item><span>Release: </span> {{machineData.data.os.release}}</q-item>
-            <q-item><span>Arch:</span> {{machineData.data.os.arch}}</q-item>
-            <q-expansion-item :header-inset-level="1" expand-separator icon="person" label="User info" default-opened>
-                <q-item><span>Username:</span> {{machineData.data.os.userInfo.username}}</q-item>
-                <q-item><span>Homedir:</span> {{machineData.data.os.userInfo.homedir}}</q-item>
-                <q-item><span>Shell:</span> {{machineData.data.os.userInfo.shell}}</q-item>
+        <q-list v-if="machineData" class="q-px-xl">
+          <q-expansion-item
+              expand-separator
+              icon="settings_system_daydream"
+              label="Operating system"
+              default-opened>
+                <q-item v-for="(val, key) of machineData.os" v-bind:key="key">
+                    <q-item-section>
+                      <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label class="text-lime">{{val}}</q-item-label>
+                    </q-item-section>
+                </q-item>
             </q-expansion-item>
 
-            <q-expansion-item :header-inset-level="1" :content-inset-level="1" expand-separator icon="network_check" label="Network interfaces" default-opened>
                 <q-expansion-item
-                    switch-toggle-side
-                    dense-toggle
-                    :label="index"
-                    :header-inset-level="1"
-                    :content-inset-level="2"
-                    v-for="(intf, index) in machineData.data.os.networkInterfaces"
-                    v-bind:key="index">
-                    <q-card>
-                      <q-card-section>
-                          <q-item
-                          v-for="(prop, ind) of machineData.data.os.networkInterfaces[index][0]"
-                          v-bind:key="ind"
-                          >
-                          {{ind}}: {{prop}}
-                      </q-item>
-                      </q-card-section>
-                    </q-card>
+                expand-separator
+                icon="person"
+                label="User info">
+                    <q-item v-for="(val, key) of machineData.userInfo" v-bind:key="key">
+                        <q-item-section>
+                          <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+                        </q-item-section>
+                        <q-item-section>
+                          <q-item-label class="text-lime">{{val}}</q-item-label>
+                        </q-item-section>
+                    </q-item>
                 </q-expansion-item>
-            </q-expansion-item>
 
-          </q-expansion-item>
+                <q-expansion-item
+                    expand-separator
+                    icon="network_check"
+                    label="Network interfaces">
+                    <q-expansion-item
+                        switch-toggle-side
+                        dense-toggle
+                        :label="intfName"
+                        :header-inset-level="1"
+                        :content-inset-level="1"
+                        v-for="(value, intfName) in machineData.networkInterfaces"
+                        v-bind:key="intfName">
+                          <q-item v-for="(val, key) of machineData.networkInterfaces[intfName]" v-bind:key="key">
+                              <q-item-section>
+                                <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+                              </q-item-section>
+                              <q-item-section>
+                                <q-item-label class="text-lime">{{val}}</q-item-label>
+                              </q-item-section>
+                          </q-item>
+                    </q-expansion-item>
+                </q-expansion-item>
         </q-list>
       </q-tab-panel>
 
       <q-tab-panel name="env">
-        <div class="text-h6">ENV</div>
-            <q-list dense bordered padding class="rounded-borders">
-              <q-item
-              clickable
-              v-ripple
-              v-for="(variable, index) of machineData.data.env"
-              :key="index">
-                <q-item-section>
-                  <b>{{index}}</b> {{variable}}
-                </q-item-section>
-              </q-item>
-            </q-list>
+        <q-list dense padding class="q-px-xl">
+          <q-item
+          clickable
+          v-ripple
+          v-for="(val, key) of machineData.env"
+          :key="key">
+            <q-item-section>
+              <q-item-label class="text-bold">{{key}}</q-item-label>
+              <q-item-label>{{val}}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
       </q-tab-panel>
     </q-tab-panels>
   </div>
@@ -73,12 +90,53 @@ export default {
   data () {
     return {
       tab: 'os',
-      machineData: ''
+      machineData: {
+        os: {
+          arch: '',
+          hostname: '',
+          platform: '',
+          release: '',
+          type: ''
+        },
+        userInfo: {},
+        networkInterfaces: {},
+        env: {}
+      }
+    }
+  },
+  methods: {
+    removeEmptyProperties (obj) {
+      const newObj = {}
+
+      Object.keys(obj).forEach(key => {
+        if (obj[key]) {
+          newObj[key] = obj[key]
+        }
+      })
+
+      return newObj
     }
   },
   created: function () {
-    console.log('Details for machine with ID:', this.$route.query.id)
-    this.machineData = this.$route.query.id
+    let queryMachine = this.$route.query.id
+    console.log('Details for machine with ID:', queryMachine.id)
+
+    for (let key in this.machineData.os) {
+      this.machineData.os[key] = queryMachine.data.os[key]
+    }
+
+    this.machineData.userInfo = queryMachine.data.os.userInfo
+
+    let networkInterfaces = queryMachine.data.os.networkInterfaces
+    for (let intf in networkInterfaces) {
+      for (let elem of networkInterfaces[intf]) {
+        if (elem.family === 'IPv4') {
+          this.machineData.networkInterfaces[intf] = elem
+        }
+      }
+    }
+
+    this.machineData.env = this.removeEmptyProperties(queryMachine.data.env)
   }
 }
 </script>
@@ -90,13 +148,14 @@ export default {
 }
 
 .q-tab-panels .q-item {
-    border: 1px solid $graniteGray;
+    border-bottom: 1px solid $graniteGray;
 }
 
 .q-expansion-item span {
     color: $mintGreen;
     font-weight: bold;
 }
+
 .q-item .column {
     color: $chromeYellow;
 }
