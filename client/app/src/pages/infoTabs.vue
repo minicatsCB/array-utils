e99e146521b00add2b05bef9b585eb238fb24666
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-10-02 00:33:56 +0200

    Fix indentation in info-tabs page

--- client/app/src/pages/infoTabs.vue
+++ client/app/src/pages/infoTabs.vue
@@ -11,67 +11,46 @@
     <q-tab-panels v-model="tab" animated>
       <q-tab-panel name="os">
         <q-list v-if="machineData" class="q-px-xl">
-          <q-expansion-item
-              expand-separator
-              icon="settings_system_daydream"
-              label="Operating system"
-              default-opened>
-                <q-item v-for="(val, key) of machineData.os" v-bind:key="key">
-                    <q-item-section>
-                      <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label class="text-lime">{{val}}</q-item-label>
-                    </q-item-section>
-                </q-item>
+          <q-expansion-item expand-separator icon="settings_system_daydream" label="Operating system" default-opened>
+            <q-item v-for="(val, key) of machineData.os" v-bind:key="key">
+              <q-item-section>
+                <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label class="text-lime">{{val}}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-expansion-item>
+
+          <q-expansion-item expand-separator icon="person" label="User info">
+            <q-item v-for="(val, key) of machineData.userInfo" v-bind:key="key">
+              <q-item-section>
+                <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label class="text-lime">{{val}}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-expansion-item>
+
+          <q-expansion-item expand-separator icon="network_check" label="Network interfaces">
+            <q-expansion-item switch-toggle-side dense-toggle :label="intfName" :header-inset-level="1" :content-inset-level="1" v-for="(value, intfName) in machineData.networkInterfaces" v-bind:key="intfName">
+              <q-item v-for="(val, key) of machineData.networkInterfaces[intfName]" v-bind:key="key">
+                <q-item-section>
+                  <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-lime">{{val}}</q-item-label>
+                </q-item-section>
+              </q-item>
             </q-expansion-item>
-
-                <q-expansion-item
-                expand-separator
-                icon="person"
-                label="User info">
-                    <q-item v-for="(val, key) of machineData.userInfo" v-bind:key="key">
-                        <q-item-section>
-                          <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
-                        </q-item-section>
-                        <q-item-section>
-                          <q-item-label class="text-lime">{{val}}</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-expansion-item>
-
-                <q-expansion-item
-                    expand-separator
-                    icon="network_check"
-                    label="Network interfaces">
-                    <q-expansion-item
-                        switch-toggle-side
-                        dense-toggle
-                        :label="intfName"
-                        :header-inset-level="1"
-                        :content-inset-level="1"
-                        v-for="(value, intfName) in machineData.networkInterfaces"
-                        v-bind:key="intfName">
-                          <q-item v-for="(val, key) of machineData.networkInterfaces[intfName]" v-bind:key="key">
-                              <q-item-section>
-                                <q-item-label class="col-3 text-lime text-capitalize">{{key}}</q-item-label>
-                              </q-item-section>
-                              <q-item-section>
-                                <q-item-label class="text-lime">{{val}}</q-item-label>
-                              </q-item-section>
-                          </q-item>
-                    </q-expansion-item>
-                </q-expansion-item>
+          </q-expansion-item>
         </q-list>
       </q-tab-panel>
 
       <q-tab-panel name="env">
         <q-list dense padding class="q-px-xl">
-          <q-item
-          clickable
-          v-ripple
-          v-for="(val, key) of machineData.env"
-          :key="key">
+          <q-item clickable v-ripple v-for="(val, key) of machineData.env" :key="key">
             <q-item-section>
               <q-item-label class="text-bold">{{key}}</q-item-label>
               <q-item-label>{{val}}</q-item-label>
