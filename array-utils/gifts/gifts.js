238c34857a61311d55da4a14873171205d2be94a
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2019-10-05 02:29:47 +0200

    Handle child spawn errors

--- array-utils/gifts/gifts.js
+++ array-utils/gifts/gifts.js
@@ -1,31 +1,40 @@
 const { spawn } = require('child_process');
 
 let gifts = {
-    catscatscatscats: function (time){
+    catscatscatscats: function(time) {
         const numberOfImages = 5;
 
         let cmd = "gsettings";
-        let defaultImgUri =`${__dirname}/assets/bg_cat_0.jpg`;
+        let defaultImgUri = `${__dirname}/assets/bg_cat_0.jpg`;
         let params = ["set", "org.gnome.desktop.background", "picture-uri", defaultImgUri];
 
         let inter = setInterval(() => {
-        	var randomIndex = Math.floor(Math.random() * numberOfImages);
+            var randomIndex = Math.floor(Math.random() * numberOfImages);
             imgUri = `${__dirname}/assets/bg_cat_${randomIndex}.jpg`;
-        	params[3] = imgUri;
-        	spawn(cmd, params);
+            params[3] = imgUri;
+            let child = spawn(cmd, params);
+            child.on('error', function(err) {
+                console.log("Error while executing child in catscatscatscats:", err);
+            });
         }, time);
     },
     wc: function() {
         let cmd = "eog";
         let uri = `${__dirname}/assets/wc.png`;
         let params= ["-f", uri];
-        spawn(cmd, params);
+        let child = spawn(cmd, params);
+        child.on('error', function(err) {
+            console.log("Error while executing child in wc:", err);
+        });
     },
     heyInternet: function() {
         let cmd = "xdg-open";
         let url = "https://cat-bounce.com/";
         let params= [url];
-        spawn(cmd, params);
+        let child = spawn(cmd, params);
+        child.on('error', function(err) {
+            console.log("Error while executing child in heyInternet:", err);
+        });
     }
 }
 
