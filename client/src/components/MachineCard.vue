54f5bc2203550bfe1870d629bae4e9b8cd110932
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-13 19:42:32 +0200

    Restore old components with little template and syntax adjustments

---
+++ client/src/components/MachineCard.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'machine-card',
+    props: {
+        hostname: String,
+        address: String
+    }
+});
+</script>
+
+<template>
+    <q-card class="my-card q-ma-md">
+        <q-card-section>
+            <q-icon name="computer" color="lime" size="md" />
+        </q-card-section>
+        <q-card-section class="summary absolute-bottom text-right text-body2">
+            <div>{{ hostname }}</div>
+            <div>{{ address }}</div>
+        </q-card-section>
+    </q-card>
+</template>
+
+<style lang="postcss">
+.q-card {
+  width: 100%;
+  max-width: 150px;
+  height: 150px;
+  max-height: 150px;
+  display: inline-block;
+  border: 1px solid $lime;
+  background-color: transparent;
+}
+
+.q-card .summary {
+  padding-top: 16px;
+  color: $lime;
+}
+</style>
+
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,59 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import TheWelcome from '../components/TheWelcome.vue'
+import MachineList from '../components/MachineList.vue'
 
 export default defineComponent({
   components: {
-    TheWelcome
+    MachineList
+  },
+  data () {
+    return {}
   }
 });
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-black" view="hHh lpr fff">
+    <div reveal class="bg-black header"> <!-- q-header -->
+      <div> <!-- q-toolbar -->
+        <h1 class="text-right text-uppercase text-h6 q-pa-xs fade-in-left"> <!-- q-toolbar-title -->
+          Hacker's dashboard
+        </h1>
+      </div>
+      <div class="flicker" indeterminate color="lime" size="xs"/> <!-- q-linear-progress -->
+    </div>
+    <MachineList />  
   </main>
-</template>
+  </template>
+
+<style>
+@keyframes flickerAnimation {
+  0%   { opacity:1; }
+  40%, 90%  { opacity:0; }
+  100% { opacity:1; }
+}
+
+@keyframes fadeIn {
+  from {
+      opacity: 0;
+      transform: translate3d(-100%, 0, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.flicker {
+    animation: flickerAnimation 5s infinite;
+}
+
+.fade-in-left {
+  animation: fadeIn 1s;
+}
+
+.header {
+    height: 52px;
+}
+</style>
