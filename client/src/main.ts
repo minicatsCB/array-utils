6da5dd9ebc815b8a74bf5041f3f84687dd8f7a8c
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-14 18:03:05 +0200

    Use Material Design Icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,12 +10,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 // TODO: This will include all components and directives regardless of whether or not you are using them
 // For tree shaking see: https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi'
+  },
 })
 
 const app = createApp(App)
