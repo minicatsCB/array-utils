93746e13e1c76c4950a530cd9802d6d0297a236a
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-14 16:56:44 +0200

    Use default layout as parent component

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: () => import('@/layouts/DefaultLayout.vue'),
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue'),
+        },
+      ],
     },
-    {
-      path: '/infoTabs',
-      name: 'infoTabs',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../components/InfoTabs.vue')
-    }
   ]
 })
 
