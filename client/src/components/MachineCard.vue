0a85915f6e343c725f967a09c1045cde9dea0ae2
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-14 18:03:23 +0200

    Revamp Machine Card component

--- client/src/components/MachineCard.vue
+++ client/src/components/MachineCard.vue
@@ -11,31 +11,16 @@ export default defineComponent({
 </script>
 
 <template>
-    <q-card class="my-card q-ma-md">
-        <q-card-section>
-            <q-icon name="computer" color="lime" size="md" />
-        </q-card-section>
-        <q-card-section class="summary absolute-bottom text-right text-body2">
-            <div>{{ hostname }}</div>
-            <div>{{ address }}</div>
-        </q-card-section>
-    </q-card>
+    <v-card
+      :title="hostname"
+      :subtitle="address"
+      prepend-icon="mdi-laptop"
+    ></v-card>
 </template>
 
 <style lang="postcss">
-.q-card {
-  width: 100%;
-  max-width: 150px;
-  height: 150px;
-  max-height: 150px;
-  display: inline-block;
-  border: 1px solid $lime;
-  background-color: transparent;
-}
-
-.q-card .summary {
-  padding-top: 16px;
-  color: $lime;
+.v-card {
+  border: 1px solid lime;
 }
 </style>
 
