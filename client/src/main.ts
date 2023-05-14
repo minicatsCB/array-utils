a4004639dd0c7b4f236b17ba30c0be2a72abc6c4
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-14 13:14:01 +0200

    Use Vuetify

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,10 +5,24 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// TODO: This will include all components and directives regardless of whether or not you are using them
+// For tree shaking see: https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.config.globalProperties.$axios=axios
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
