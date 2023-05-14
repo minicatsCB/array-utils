b4db60c4c9cc66bd48993028e9f2bec853d82769
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-14 13:26:49 +0200

    Remove unnecessary components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,15 @@
 <script lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import { defineComponent } from 'vue';
 
-export default defineComponent({
-  components: {
-    HelloWorld
-  }
-});
+export default defineComponent({});
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div class="wrapper"> 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
