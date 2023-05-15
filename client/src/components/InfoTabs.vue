7c23942fa2dca0f71034ac637ca8ec3a41888d58
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-15 18:02:54 +0200

    Revamp info tabs component

--- client/src/components/InfoTabs.vue
+++ client/src/components/InfoTabs.vue
@@ -16,90 +16,49 @@ export default {
 </script>
 
 <template>
-    <div class="q-pa-md">
-        <div class="q-gutter-y-md">
-            <q-tabs v-model="tab" dense class="text-lime" active-color="lime" indicator-color="lime" align="center"
-                narrow-indicator>
-                <q-tab name="os" label="OS"></q-tab>
-                <q-tab name="env" label="ENV"></q-tab>
-            </q-tabs>
+    <v-container>
+        <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
+            <v-tab :value="1">NETWORK INTERFACES</v-tab>
+            <v-tab :value="2">OS</v-tab>
+            <v-tab :value="3">ENV</v-tab>
+            <v-tab :value="4">USER INFO</v-tab>
+        </v-tabs>
+        <v-window v-model="tab">
+            <v-window-item :key="1" :value="1">
+                <v-container fluid>
+                    <v-list lines="one">
+                        <v-list-item v-for="(val, key) of machine.os" :key="key" :title="`Title ${key}`"
+                            :subtitle="`Subtitle ${val}`"></v-list-item>
+                    </v-list>
+                </v-container>
+            </v-window-item>
 
-            <q-separator class="bg-lime"></q-separator>
+            <v-window-item :key="2" :value="2">
+                <v-container fluid>
+                    <v-list lines="one">
+                        <v-list-item v-for="(val, key) of machine.userInfo" :key="key" :title="`Title ${key}`"
+                            :subtitle="`Subtitle ${val}`"></v-list-item>
+                    </v-list>
+                </v-container>
+            </v-window-item>
 
-            <q-tab-panels v-model="tab" animated>
-                <q-tab-panel name="os">
-                    <q-list v-if="machine" class="q-px-xl">
-                        <q-expansion-item expand-separator icon="settings_system_daydream" label="Operating system"
-                            default-opened>
-                            <q-item v-for="(val, key) of machine.os" v-bind:key="key">
-                                <q-item-section>
-                                    <q-item-label class="col-3 text-lime text-capitalize">{{ key }}</q-item-label>
-                                </q-item-section>
-                                <q-item-section>
-                                    <q-item-label class="text-lime">{{ val }}</q-item-label>
-                                </q-item-section>
-                            </q-item>
-                        </q-expansion-item>
+            <v-window-item :key="3" :value="3">
+                <v-container fluid>
+                    <v-list lines="one">
+                        <v-list-item v-for="(val, key) of machine.env" :key="key" :title="`Title ${key}`"
+                            :subtitle="`Subtitle ${val}`"></v-list-item>
+                    </v-list>
+                </v-container>
+            </v-window-item>
 
-                        <q-expansion-item expand-separator icon="person" label="User info">
-                            <q-item v-for="(val, key) of machine.userInfo" v-bind:key="key">
-                                <q-item-section>
-                                    <q-item-label class="col-3 text-lime text-capitalize">{{ key }}</q-item-label>
-                                </q-item-section>
-                                <q-item-section>
-                                    <q-item-label class="text-lime">{{ val }}</q-item-label>
-                                </q-item-section>
-                            </q-item>
-                        </q-expansion-item>
-
-                        <q-expansion-item expand-separator icon="network_check" label="Network interfaces">
-                            <q-expansion-item switch-toggle-side dense-toggle :label="intfName" :header-inset-level="1"
-                                :content-inset-level="1" v-for="(value, intfName) in machine.networkInterfaces"
-                                v-bind:key="intfName">
-                                <q-item v-for="(val, key) of machine.networkInterfaces[intfName]" v-bind:key="key">
-                                    <q-item-section>
-                                        <q-item-label class="col-3 text-lime text-capitalize">{{ key }}</q-item-label>
-                                    </q-item-section>
-                                    <q-item-section>
-                                        <q-item-label class="text-lime">{{ val }}</q-item-label>
-                                    </q-item-section>
-                                </q-item>
-                            </q-expansion-item>
-                        </q-expansion-item>
-                    </q-list>
-                </q-tab-panel>
-
-                <q-tab-panel name="env">
-                    <q-list dense padding class="q-px-xl">
-                        <q-item clickable v-ripple v-for="(val, key) of machine.env" :key="key">
-                            <q-item-section>
-                                <q-item-label class="text-bold">{{ key }}</q-item-label>
-                                <q-item-label>{{ val }}</q-item-label>
-                            </q-item-section>
-                        </q-item>
-                    </q-list>
-                </q-tab-panel>
-            </q-tab-panels>
-        </div>
-    </div>
+            <v-window-item :key="4" :value="4">
+                <v-container fluid>
+                    <v-list lines="one" v-for="(value, intfName) in machine.networkInterfaces">
+                        <v-list-item v-for="(val, key) of machine.networkInterfaces[intfName]" :key="key"
+                            :title="`Title ${key}`" :subtitle="`Subtitle ${val}`"></v-list-item>
+                    </v-list>
+                </v-container>
+            </v-window-item>
+        </v-window>
+    </v-container>
 </template>
-
-<style lang="postcss">
-.q-tab-panels {
-    background-color: transparent;
-    color: $lime;
-}
-
-.q-tab-panels .q-item {
-    border-bottom: 1px solid $graniteGray;
-}
-
-.q-expansion-item span {
-    color: $mintGreen;
-    font-weight: bold;
-}
-
-.q-item .column {
-    color: $chromeYellow;
-}
-</style>
