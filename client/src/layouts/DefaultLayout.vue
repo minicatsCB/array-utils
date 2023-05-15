f877c46f5088b53634ab306661666f0b3a3ac182
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-15 19:53:25 +0200

    Show current path

--- client/src/layouts/DefaultLayout.vue
+++ client/src/layouts/DefaultLayout.vue
@@ -5,7 +5,7 @@
             <img alt="Vue logo" src="@/assets/logo.svg" class="app-logo" />
             <v-spacer></v-spacer>
         </v-toolbar>
-        <v-breadcrumbs :items="['Foo', 'Bar', 'Fizz']"></v-breadcrumbs>
+        <v-breadcrumbs :items="[`${$route.path}`]"></v-breadcrumbs>
 
         <v-main>
             <router-view />
