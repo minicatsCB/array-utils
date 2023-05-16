a6141e0857a37f1f782b4ee3675efb19a581311f
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-16 18:37:04 +0200

    Refs #2: Add more typings

--- array-utils/typings/array-utils/index.d.ts
+++ array-utils/typings/array-utils/index.d.ts
@@ -1,3 +1,5 @@
+import { NetworkInterfaceInfoIPv4, UserInfo } from "node:os";
+
 declare module "array-utils" {
     interface OsDetails {
         arch: string;
@@ -6,4 +8,9 @@ declare module "array-utils" {
         release: string;
         type: string;
     }
+
+    type UserDetails = UserInfo<string>
+
+    type NetworkInterfacesDetails = NodeJS.Dict<NetworkInterfaceInfoIPv4>
+    type EnvDetails = NodeJS.Dict<string>
 }
