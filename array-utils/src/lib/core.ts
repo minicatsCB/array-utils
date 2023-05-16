e3eddf1615eeca218fa9e6d6e04ab24d9566d61e
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-05-16 20:34:40 +0200

    Refs #2: rewrite core using TypeScript

---
+++ array-utils/src/lib/core.ts
@@ -0,0 +1,46 @@
+import * as http from "http";
+
+function onData(chunk: any): void {
+	console.log(`BODY: ${chunk}`);
+}
+
+function onEnd():void {
+	console.log('No more data in response.');
+}
+
+function onResponse(res: http.IncomingMessage): void {
+	console.log(`Response status: ${res.statusCode}`);
+	console.log(`Response headers: ${JSON.stringify(res.headers)}`);
+
+	res.setEncoding('utf8');
+	
+	res.on('data', onData);
+	res.on('end', onEnd);
+}
+
+function onError(e: Error): void {
+	console.error(`problem with request: ${e.message}`);
+}
+
+function sendData(data: string, pluginType: string, machineId: string): void {
+	const options: http.RequestOptions = {
+		hostname: "http://localhost",	// NOTE: temporary harcoded to localhost for testing purposes
+		port: 3000,
+		path: '/',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(data),
+			'X-Plugin-Type': pluginType,
+			'X-Machine-Id': machineId
+		}
+	};
+	const req: http.ClientRequest = http.request(options, onResponse);
+
+	req.on('error', onError);
+
+	req.write(data);
+	req.end();
+}
+
+export { sendData };
