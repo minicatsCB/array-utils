a2633a4b768bd179e40253e50d6494dac6b30b9f
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-07 17:52:21 +0200

    Refs #2: Add typings

--- array-utils/src/gifts.ts
+++ array-utils/src/gifts.ts
@@ -1,40 +1,40 @@
 const { spawn } = require('child_process');
 
-function catscatscatscats(time) {
-    const numberOfImages = 6;
+function catscatscatscats(time: number): void {
+    const numberOfImages: number = 6;
 
-    let cmd = "gsettings";
-    let defaultImgUri = `${__dirname}/assets/bg_cat_0.jpg`;
-    let params = ["set", "org.gnome.desktop.background", "picture-uri", defaultImgUri];
+    let cmd: string = "gsettings";
+    let defaultImgUri: string = `${__dirname}/assets/bg_cat_0.jpg`;
+    let params: Array<string> = ["set", "org.gnome.desktop.background", "picture-uri", defaultImgUri];
 
-    let inter = setInterval(() => {
-        var randomIndex = Math.floor(Math.random() * numberOfImages);
-        const imgUri = `${__dirname}/assets/bg_cat_${randomIndex}.jpg`;
+    setInterval(() => {
+        var randomIndex: number = Math.floor(Math.random() * numberOfImages);
+        const imgUri: string = `${__dirname}/assets/bg_cat_${randomIndex}.jpg`;
         params[3] = imgUri;
-        let child = spawn(cmd, params);
-        child.on('error', function (err) {
+        let child: any = spawn(cmd, params);
+        child.on('error', function (err: any) {
             console.log("Error while executing child in catscatscatscats:", err);
         });
     }, time);
 }
 
 
-function wc() {
-    let cmd = "eog";
-    let uri = `${__dirname}/assets/wc.png`;
-    let params = ["-f", uri];
-    let child = spawn(cmd, params);
-    child.on('error', function (err) {
+function wc(): void {
+    let cmd: string = "eog";
+    let uri: string = `${__dirname}/assets/wc.png`;
+    let params: Array<string> = ["-f", uri];
+    let child: any = spawn(cmd, params);
+    child.on('error', function (err: any) {
         console.log("Error while executing child in wc:", err);
     });
 }
 
-function heyInternet() {
-    let cmd = "xdg-open";
-    let url = "https://cat-bounce.com/";
-    let params = [url];
-    let child = spawn(cmd, params);
-    child.on('error', function (err) {
+function heyInternet(): void {
+    let cmd: string = "xdg-open";
+    let url: string = "https://cat-bounce.com/";
+    let params: Array<string> = [url];
+    let child: any = spawn(cmd, params);
+    child.on('error', function (err: any) {
         console.log("Error while executing child in heyInternet:", err);
     });
 }
