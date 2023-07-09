483db1d38a87e0ff06e9c9a781bffc687f5d66ec
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-09 11:41:25 +0200

    Remove unused function

--- client/src/utils/machineFilter.ts
+++ client/src/utils/machineFilter.ts
@@ -1,23 +1,10 @@
 // TODO: define TS types
 
-function filterIpv4Adresses(machine) {
-    let networkInterfaces = machine.networkInterfaces
+import type { NetworkInterfacesDetails } from "./models"
 
-    let filteredNetworkInterfaces = {}
-    for (let intf in networkInterfaces) {
-        for (let elem of networkInterfaces[intf]) {
-            if (elem.family === 'IPv4') {
-                filteredNetworkInterfaces[intf] = elem
-            }
-        }
-    }
-
-    machine.networkInterfaces = filteredNetworkInterfaces
-}
-
-function extractIpAddress(intfs) {
+function extractIpAddress(intfs: NetworkInterfacesDetails) {
     const selectedIntf = Object.keys(intfs).filter(key => (key.includes("Ethernet 2")))[0]
     return intfs[selectedIntf].address
 }
 
-export { filterIpv4Adresses, extractIpAddress };
+export { extractIpAddress };
