de975b450707e537af88686ba032952a89043825
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-09 11:35:05 +0200

    Replace Quasar with Vuetify component

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -10,6 +10,6 @@ export default {
             <img src="~assets/sad.svg" style="width:30vw;max-width:150px;">
         </p>
         <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
-        <q-btn color="secondary" style="width:200px;" @click="$router.push('/')">Go back</q-btn>
+        <v-btn color="secondary" style="width:200px;" @click="$router.push('/')">Go back</v-btn>
     </div>
 </template>
