64071eb92f731d433d03110748e29c3e3289bb0b
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-06-18 19:47:22 +0200

    feat(server): move middleware to its own directory

--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const dataRouter = require("./routes/data");
+const corsMiddleware = require("./middleware/cors");
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(corsMiddleware);
 
 app.use('/data', dataRouter);
 
