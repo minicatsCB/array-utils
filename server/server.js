f98db86dbbb6961135cdea98b893b96aa7444bb3
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-06-18 19:50:18 +0200

    feat(server): log every request into console

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const dataRouter = require("./routes/data");
 const corsMiddleware = require("./middleware/cors");
+const loggerMiddleware = require("./middleware/logger");
 
 app.use(express.json());
+app.use(loggerMiddleware);
 app.use(corsMiddleware);
 
 app.use('/data', dataRouter);
