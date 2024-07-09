6e8e31e08e94e0e5864c9729e7cbc35aba5e8053
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-07-09 20:16:58 +0200

    fix(server): for Docker compose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from "./app/app.js";
 
-const port = process.env.port || 4000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`);
 });
