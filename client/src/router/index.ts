37b96485fa378130fcb35a19686450ee7a43594c
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2024-07-13 15:03:49 +0200

    chore(client): improve router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,30 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import Home from '@/views/HomeView.vue';
+import Details from '@/views/DetailsView.vue';
+import NotFound from '@/views/NotFoundView.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/machines',
+  },
+  {
+    path: '/machines',
+    redirect: '/machines/home',
+    component: DefaultLayout,
+    children: [
+      {
+        path: 'home/',
+        name: 'Home',
+        component: Home,
+      },
+      {
+        path: 'details/:id',
+        name: 'About',
+        component: Details,
+      },
+    ]
+  },
+  { path: '/:catchAll(.*)', name: 'NotFound', component: NotFound },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/layouts/DefaultLayout.vue'),
-      redirect: 'home',
-      children: [
-        {
-          path: 'home',
-          name: 'Home',
-          // route level code-splitting
-          // this generates a separate chunk (about.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue'),
-        },
-        {
-          path: 'details/:id',
-          name: 'Details',
-          component: () => import(/* webpackChunkName: "infoTabs" */ '@/views/DetailsView.vue'),
-          props: true
-        }
-      ],
-    },
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
